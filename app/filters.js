dfbd05b18d1b18fbbccbb9207e56e9ed72d3c685
monsterlimo <mattnixon79@googlemail.com>
2022-01-05 09:57:33 +0000

    first push

--- app/filters.js
+++ app/filters.js
@@ -38,6 +38,16 @@ module.exports = function (env) {
 
   ------------------------------------------------------------------ */
 
+  filters.toMonth = function(x){ 
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];   
+    if (x > 0){ return months[x - 1]; // returns date as per month      
+    } else {
+            return x ;      
+    }}     
+    filters.toMoney = function(x){  return("£" + x );
+    //TO ADD - case to handle nothing being there 
+  }
+
   /* ------------------------------------------------------------------
     keep the following line to return your filters to the app
   ------------------------------------------------------------------ */
--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,24 @@
 const express = require('express')
 const router = express.Router()
+const radioButtonRedirect = require('radio-button-redirect')
 
-// Add your routes here - above the module.exports line
+router.use(radioButtonRedirect)
+
+// GET SPRINT NAME - useful for relative templates
+router.use('/', (req, res, next) => {
+    res.locals.currentURL = req.originalUrl; //current screen
+    res.locals.prevURL = req.get('Referrer'); // previous screen
+    req.folder = req.originalUrl.split('/')[1]; //folder, e.g. 'current'
+    req.subfolder = req.originalUrl.split('/')[2]; //sub-folder e.g. 'service'
+    res.locals.folder = req.folder; // what folder the url is
+    res.locals.subfolder = req.subfolder; // what subfolder the URL is in
+    console.log('folder : ' + res.locals.folder + ', subfolder : ' + res.locals.subfolder  );
+    console.log('previous page is: ' + res.locals.prevURL + " and current page is " + req.url + " " + res.locals.currentURL );
+    next();
+  });
+
+// Start folder specific routes
+// current sprint, remember to add older sprint when adding a new folder!
+router.use('/current', require('./views/current/_routes'));
 
 module.exports = router
